f80d4dd71f7def76880e8f3ea915978ce56efcfb
Luka Čehovin <luka.cehovin@gmail.com>
2019-09-26 13:27:31 +0200

    Adding new helper publisher.

--- include/echolib/helpers.h
+++ include/echolib/helpers.h
@@ -40,6 +40,46 @@ private:
 
 };
 
+template<typename T> class ObjectPublisher: public TypedPublisher<T>, public Watcher, public std::enable_shared_from_this<ObjectPublisher<T> > {
+public:
+    ObjectPublisher(SharedClient client, const string &alias, T& value) : TypedPublisher<T>(client, alias), Watcher(client, alias), value(value) {
+
+        subscribers = 0;
+
+    }
+
+    using TypedPublisher<T>::send;
+
+    virtual ~ObjectPublisher() {}
+
+    virtual void on_event(SharedDictionary message) {
+        string type = message->get<string>("type", "");
+
+        if (type == "subscribe" || type == "unsubscribe" || type == "summary") {
+            int s = message->get<int>("subscribers", 0);
+            if (s > subscribers) {
+                send(value);
+            }
+            subscribers = s;
+        }
+    }
+
+    void update(T& new_value) {
+
+        value = new_value;
+
+        send(value);
+
+    }
+
+private:
+
+    int subscribers;
+
+    T value;
+
+};
+
 }
 
 // Optional protobuf support

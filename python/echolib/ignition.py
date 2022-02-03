5f58f8420f1ab363677f325d426b188c31027b8f
Luka Cehovin Zajc <luka.cehovin@gmail.com>
2022-02-03 16:58:08 +0100

    Fixing ignition plugin

--- python/echolib/ignition.py
+++ python/echolib/ignition.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 
 import os, sys
-from ignition import mergevars
+from ignition.program import mergevars
 from ignition.plugin import Plugin
 
 class Mapping(Plugin):
@@ -10,13 +10,12 @@ class Mapping(Plugin):
     def __init__(self):
         super().__init__()
 
-    def on_program_init(self, program, **kwargs):
-        mapping = kwargs.get("remap", {})
+    def on_program_init(self, program):
+        mapping = program.auxiliary.get("remap", {})
         if not type(mapping) is dict:
             return
         maplist = []
         for k, v in mapping.items():
             maplist.append("%s=%s" % (k, v))
 
-        program.environment = mergevars({"ECHOLIB_MAP" : ";".join(maplist)}, program.environment)
-
+        program.environment["ECHOLIB_MAP"] = ";".join(maplist)
